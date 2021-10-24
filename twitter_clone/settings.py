e8c1fc78ccc9ee87357f83433a0feab1049d4617
yandys alfonso <yandys.alfonso86@gmail.com>
2021-10-24 18:16:05 -0500

    allow heroku host

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0g(p7l82bmfdxd-m3qe09hfawhb^2j7#zn*q&=!bi&gk5_fkn!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["twitterclone-yandys.herokuapp.com"]
 
 
 # Application definition
